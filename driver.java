e0190a79224174ecbb6dff20e5c63dddec7bd679
joshzelinsky <joshua.zelinsky@cuw.edu>
2015-03-27 20:20:10 -0500

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,22 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.Scanner;
-
 public class driver
 {
+	static LinkedList<ConnectedClient> theClients = new LinkedList<ConnectedClient>();
 	public static void main(String[] args) throws Exception
 	{
-		//Just added this line
+		//connect to the server
+		Socket theServer = new Socket("localhost",1234);
+		
+		ClientCommunicationProtocol ccp = new ClientCommunicationProtocol(theServer);
 		ServerSocket ss = new ServerSocket(1234);
-		int count = 0;
 		while(true)
 		{
 			System.out.println("Waiting...");
 			Socket connection = ss.accept();
-			ResponseThread rt = new ResponseThread(connection);
-			System.out.println("Connected!");
+			ConnectedClient cc = new ConnectedClient(connection);
+			driver.theClients.add(cc);
+			ResponseThread rt = new ResponseThread(cc);
 			rt.start();
+			System.out.println("Connected!");
 		}
 	}
 }
